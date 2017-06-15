32342b99c6c2c9e9cc9d9e3ac3986e9d4df0111f
Cameron Samuels <thecameron@playbclick.com>
2017-06-15 17:24:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ var ut; function text() { clearTimeout(ut); ut = setTimeout(update, 500) } //fix
 function update() { //updates text
 	canvas.style.background = "url(" + canvas.toDataURL() + ") no-repeat 100%/cover";
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-// 	ctx.drawImage(img, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
         ctx.translate(canvas.width / 2, canvas.height / 2);
         ctx.rotate(r);
         ctx.translate(-canvas.width / 2, -canvas.height / 2);
-	ctx.drawImage(img,0,0);
+	//ctx.drawImage(img,0,0);
+ctx.drawImage(img, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
 	ctx.rotate(0);
 	ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
 	ctx.strokeText(document.querySelectorAll('input')[2].value, canvas.width / 2, canvas.height - (canvas.height / 17));
