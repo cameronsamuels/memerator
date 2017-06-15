7319c0b44bb6b255c27c1c331df6cf25641e3883
Cameron Samuels <thecameron@playbclick.com>
2017-06-15 17:06:38 -0500

    Cleaned up code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var d = function(i) { return document.querySelector(i) }, img, canvas = d('canvas');
-canvas.width = 1024, canvas.height = 1024, ctx = canvas.getContext("2d"); // canvas dimensions
+canvas.width = 1024, canvas.height = 1024, ctx = canvas.getContext("2d"), ctx.font = (canvas.height / 8) + "px impac";
+ctx.textAlign = "center", ctx.lineWidth = canvas.height / 64, ctx.fillStyle = "#fff", ctx.strokeStyle = '#000'; // canvas dimensions
 d('[type=file]').addEventListener("change", function(event) {
 	var url = this.value, ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase(); // file extension checker. proceed if correct.
 	if (d('input').files && d('input').files[0] && (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) {
@@ -11,12 +12,6 @@ d('[type=file]').addEventListener("change", function(event) {
 				canvas.height = img.height;
 				ctx.drawImage(img, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
 				d('form div:nth-child(2)').id = '';
-				//if (d("#after").innerHTML == "") d("form").innerHTML = d("form").innerHTML.replace('<div id="after"></div>', '') + '<div class="waves-effect waves-light btn"><a download="meme.png">Download</a></div><div id="after"><p>Top text:</p><input type="text" onkeyup="text()" placeholder="One can simply make" /><p></p><p>Bottom text:</p><input onkeyup="text()" type="text" placeholder="memes with memerator" /></div>';
-				ctx.font = (canvas.height / 8) + "px impac";
-				ctx.textAlign = "center";
-				ctx.lineWidth = canvas.height / 64;
-				ctx.fillStyle = "#fff";
-				ctx.strokeStyle = '#000';
 				ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
 				ctx.strokeText(document.querySelectorAll('input')[2].value, canvas.width / 2, canvas.height - (canvas.height / 17));
 				ctx.fillText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
@@ -37,11 +32,6 @@ function update() { //updates text
 	canvas.width = img.width;
 	canvas.height = img.height;
 	ctx.drawImage(img, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
-	ctx.font = (canvas.height / 8) + "px impac";
-	ctx.textAlign = "center";
-	ctx.lineWidth = canvas.height / 64;
-	ctx.fillStyle = "#fff";
-	ctx.strokeStyle = '#000';
 	ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
 	ctx.strokeText(document.querySelectorAll('input')[2].value, canvas.width / 2, canvas.height - (canvas.height / 17));
 	ctx.fillText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
@@ -54,26 +44,11 @@ function rotate() { // handles rotation on click
         ctx.rotate(1.57);
         ctx.translate(-canvas.width / 2, -canvas.height / 2);
 	ctx.drawImage(img,0,0);
-	ctx.font = (canvas.height / 8) + "px impac";
-	ctx.textAlign = "center";
-	ctx.lineWidth = canvas.height / 64;
-	ctx.fillStyle = "#fff";
-	ctx.strokeStyle = '#000';
 	ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
 	ctx.strokeText(document.querySelectorAll('input')[2].value, canvas.width / 2, canvas.height - (canvas.height / 17));
 	ctx.fillText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
 	ctx.fillText(document.querySelectorAll('input')[2].value, canvas.width / 2, canvas.height - (canvas.height / 17));
 	d('a').href = canvas.toDataURL();
-	
-//         var newImage = new Image();
-//         newImage.src = canvas.toDataURL("image/png");
-//         return newImage;
-	
-// 	if ("createEvent" in document) {
-// 		var evt = document.createEvent("HTMLEvents");
-// 		evt.initEvent("change", false, true);
-// 		d('[type=file]').dispatchEvent(evt);
-// 	} else d('[type=file]').fireEvent("onchange");
 	// draw the image
 	// since the context is rotated, the image will be rotated also
 	// we’re done with the rotating so restore the unrotated context
