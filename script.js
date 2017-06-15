5338c28528d5392bfd66c4acec5506f2ae837853
Cameron Samuels <thecameron@playbclick.com>
2017-06-14 20:35:32 -0500

    Create script.js

--- script.js
+++ script.js
@@ -45,7 +45,28 @@ function text(event) {
 		evt.initEvent("change", false, true);
 		$('[type=file]').dispatchEvent(evt);
 	} else $('[type=file]').fireEvent("onchange");
-	
+}
+function rotate() {
+	var canvas = $('canvas'), context = canvas.getContext("2d");
+	    context.clearRect(0,0,canvas.width,canvas.height);
+
+	    // save the unrotated context of the canvas so we can restore it later
+	    // the alternative is to untranslate & unrotate after drawing
+	    context.save();
+
+	    // move to the center of the canvas
+	    context.translate(canvas.width/2,canvas.height/2);
+
+	    // rotate the canvas to the specified degrees
+	    context.rotate(degrees*Math.PI/180);
+
+	    // draw the image
+	    // since the context is rotated, the image will be rotated also
+	    context.drawImage(image,-image.width/2,-image.width/2);
+
+	    // we’re done with the rotating so restore the unrotated context
+	    context.restore();
+}	
 }
 document.querySelectorAll('[type=text]')[0].addEventListener("keyup", text, false);
 document.querySelectorAll('[type=text]')[1].addEventListener("keyup", text, false);
