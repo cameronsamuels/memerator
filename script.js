0c899595d7950c9a1b800d972753e1ce754e9a3c
Cameron Samuels <thecameron@playbclick.com>
2017-06-15 20:16:17 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -30,25 +30,11 @@ var ut; function text() { clearTimeout(ut); ut = setTimeout(update, 500) } //fix
 function update() { //updates text
 	canvas.style.background = "url(" + canvas.toDataURL() + ") no-repeat 100%/cover";
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
-	// translate to center-canvas 
-    	// the origin [0,0] is now center-canvas
     	ctx.translate(canvas.width / 2, canvas.height / 2);
-	
-	// roate the canvas by +90% (==Math.PI/2)
 	ctx.rotate(r);
-
-	// draw the signature
-	// since images draw from top-left offset the draw by 1/2 width & height
 	ctx.drawImage(img, -img.width / 2, -img.height / 2);
-
-	// un-rotate the canvas by -90% (== -Math.PI/2)
 	ctx.rotate(-r);
-
-	// un-translate the canvas back to origin==top-left canvas
 	ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
-	
 	ctx.textAlign = "center", ctx.fillStyle = "#fff", ctx.strokeStyle = '#000',
 	ctx.font = (canvas.height / 8) + "px impac", ctx.lineWidth = canvas.height / 64;
 	ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
@@ -60,34 +46,22 @@ function update() { //updates text
 function rotate() { // handles rotation on click
 	if ($('input[type=text]').css('visibility')=='hidden') return;
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-// 	r += 1.5708;
 	rt = rt==1?0:1;
-	if (rt == 1) {
-		canvas.width = img.height;
-		canvas.height = img.width;
-	}
-	else {
-		canvas.width = img.width;
-		canvas.height = img.height;
-	}
+	if (rt == 1) { canvas.width = img.height; canvas.height = img.width }
+	else { canvas.width = img.width; canvas.height = img.height }
 	// translate to center-canvas 
     	// the origin [0,0] is now center-canvas
     	ctx.translate(canvas.width / 2, canvas.height / 2);
-	
 	// roate the canvas by +90% (==Math.PI/2)
 	r += 1.5708;
 	ctx.rotate(r);
-
 	// draw the signature
 	// since images draw from top-left offset the draw by 1/2 width & height
 	ctx.drawImage(img, -img.width / 2, -img.height / 2);
-
 	// un-rotate the canvas by -90% (== -Math.PI/2)
 	ctx.rotate(-r);
-
 	// un-translate the canvas back to origin==top-left canvas
 	ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
 	ctx.textAlign = "center", ctx.fillStyle = "#fff", ctx.strokeStyle = '#000',
 	ctx.font = (canvas.height / 8) + "px impac", ctx.lineWidth = canvas.height / 64;
 	ctx.strokeText(document.querySelectorAll('input')[1].value, canvas.width / 2, canvas.height / 7);
