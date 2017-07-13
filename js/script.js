d8403a58445d504ace82b9149fe5d404141be3f7
Cameron Samuels <satchmosanders@gmail.com>
2017-07-13 10:30:04 -0500

    Combined events using jquery

--- js/script.js
+++ js/script.js
@@ -23,8 +23,7 @@ d('[type=file]').addEventListener("change", function(event) {
                 d('a').href = canvas.toDataURL();
                 $('.row').css('visibility', 'visible');
                 $('#upload').html('Replace' + $('#upload').html().replace('Upload', ''));
-                $('.jscolor').click(function(){$('body>div:last-child').mouseup(function(){setTimeout(update,500)})})
-                $('.jscolor').click(function(){$('body>div:last-child').on('touchend', function(){setTimeout(update,500)})})
+                $('.jscolor').click(function(){$('body>div:last-child').on('touchend mouseup', function(){setTimeout(update,500)})});
             }
             img.src = e.target.result;
             canvas.style.background = "url(" + img.src + ") no-repeat 100%/cover";
